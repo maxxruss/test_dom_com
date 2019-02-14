43660d3a1a7a5ee0744201c7a951cfe0b5326741
max <ig_max@mail.ru>
2019-02-14 18:24:35 +0700

    .htaccess

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'NE8AvaJY98tEGErx-RozOA6dDR35lUG1',
+            'baseUrl'=> '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
